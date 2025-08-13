755b2bef0d3b63daf8585a20c0efe7985033e2c8
Angel-Zeyu <fantasybzy@163.com>
2025-08-13 11:18:08 +0800

    new dev

--- ex_decorate.py
+++ ex_decorate.py
@@ -1,10 +1,14 @@
 from functools import lru_cache
 
-@lru_cache(maxsize=None)
-def add(a, b):
-   print("add...")
-   return a + b
-print(add(2, 3))
-print(add(3,5))
+# @lru_cache(maxsize=None)
+# def add(a, b):
+#    print("add...")
+#    return a + b
+# print(add(2, 3))
+# print(add(3,5))
 
-print(add(2, 3))
+# print(add(2, 3))
+
+class abb
+for i in 10:
+    print("11")
